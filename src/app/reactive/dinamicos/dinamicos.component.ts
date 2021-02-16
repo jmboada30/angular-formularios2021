b0b193c2fe277f0684e4047e3f551e518d24cfc5
Joel Boada Srv Worknet <jmboada30@gmail.com>
2021-02-16 16:47:00 -0500

    init

---
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [],
+})
+export class DinamicosComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array(
+      [
+        ['MarioBros', Validators.required],
+        ['Pokemon', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
+
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  campoEsInvalid(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  guardar() {
+    this.miFormulario.markAllAsTouched();
+    if (this.miFormulario.invalid) return;
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
+}
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [],
+})
+export class SidemenuComponent {
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './template/basicos',
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './template/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches',
+    },
+  ];
+
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './reactive/basicos',
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches',
+    },
+  ];
+}
